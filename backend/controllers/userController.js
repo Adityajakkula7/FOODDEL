1eb49e7f938a8ddd06f6661037886e819ea9bea8
Adityajakkula7 <159045832+Adityajakkula7@users.noreply.github.com>
2025-06-07 20:05:28 +0530

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -28,7 +28,7 @@ const loginUser = async (req,res) => {
 }
 
     const createToken = (id) => {
-        return jwt.sign({id},process.env.JWT_secret);
+        return jwt.sign({id},process.env.JWT_SECRET);
     }
 
 //register user
