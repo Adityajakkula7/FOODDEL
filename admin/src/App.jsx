ff75c6d2caae6dfbc3e6e7dafe21d6d93ca99d87
Adityajakkula7 <159045832+Adityajakkula7@users.noreply.github.com>
2025-06-07 19:31:29 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css'
 
 
 const App = () => {
-  const url = "http://localhost:4000"
+  const url = "https://fooddel-backend-43zi.onrender.com"
 
   return (
     <div>
